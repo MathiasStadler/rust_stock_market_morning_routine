3cf5e7f98b9081e28026f210b43206c532bd9ee1
Mathias Stadler <stadler-mathias@web.de>
2025-04-29 15:41:30 +0200

    save struct example

---
+++ examples/dyn_boxing_errors_w_result.rs
@@ -0,0 +1,47 @@
+// FROM HERE - just for understanding
+// https://doc.rust-lang.org/rust-by-example/error/multiple_error_types/boxing_errors.html
+
+use std::error;
+use std::fmt;
+
+// Change the alias to use `Box<dyn error::Error>`.
+#[allow(dead_code)]
+type Result<T> = std::result::Result<T, Box<dyn error::Error>>;
+
+#[derive(Debug, Clone)]
+struct EmptyVec;
+
+impl fmt::Display for EmptyVec {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "invalid first item to double")
+    }
+}
+
+impl error::Error for EmptyVec {}
+
+fn double_first(vec: Vec<&str>) -> std::result::Result<i32, Box<dyn error::Error>> {
+    vec.first()
+        .ok_or_else(|| EmptyVec.into()) // Converts to Box
+        .and_then(|s| {
+            s.parse::<i32>()
+                .map_err(|e| e.into()) // Converts to Box
+                .map(|i| 2 * i)
+        })
+}
+
+fn print(result: std::result::Result<i32, Box<dyn error::Error>>) {
+    match result {
+        Ok(n) => println!("The first doubled is {}", n),
+        Err(e) => println!("Error: {}", e),
+    }
+}
+
+fn main() {
+    let numbers = vec!["33", "93", "18"];
+    // let empty = vec![];
+    // let strings = vec!["tofu", "93", "18"];
+
+    print(double_first(numbers));
+    // print(double_first(empty));
+    // print(double_first(strings));
+}
